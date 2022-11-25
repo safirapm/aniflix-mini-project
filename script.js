29e47570b033c2d1ad87b5ac6229d880bef26eae
safirapm <safira.paramita.m@gmail.com>
2022-11-25 23:17:13 +0700

    js-update

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-  // Navbar
-  let nav = document.querySelector('nav');
-  window.addEventListener('scroll', function(){
-      if(window.pageYOffset >20) {
-      nav.classList.add('nav', 'shadow');
+// Navbar
+let nav = document.querySelector("nav");
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 30) {
+    nav.classList.add("nav", "shadow");
   } else {
-      nav.classList.remove('nav', 'shadow');
+    nav.classList.remove("nav", "shadow");
   }
-  })
+});
 
-  // Sign In Password
-  const showPassword = document.querySelector
-    ("#show-password");
-    const passwordField = document.querySelector
-    ("#password");
+// Sign In Password
+const showPassword = document.querySelector("#show-password");
+const passwordField = document.querySelector("#password");
 
-    showPassword.addEventListener("click", function(){
-        this.classList.toggle("bi-eye-slash-fill");
-        const type = passwordField.getAttribute("type")
-        === "password" ? "text" : "password"
-        passwordField.setAttribute("type", type);
-    })
+showPassword.addEventListener("click", function () {
+  this.classList.toggle("bi-eye-slash-fill");
+  const type =
+    passwordField.getAttribute("type") === "password" ? "text" : "password";
+  passwordField.setAttribute("type", type);
+});
+
+// Name timeout (name change)
+let elementName = document.getElementById("aniflix");
+
+setTimeout(() => {
+  document.getElementsByClassName("navbar-brand")[0].innerHTML = "a-flix";
+}, 3000);
